66dd0115591829d9e131ff875b356b81fc1ac04b
Marlon DC <marlondc@thisismn.com>
2017-02-21 14:26:53 +0000

    user

--- public/js/global.js
+++ public/js/global.js
@@ -1,7 +1,16 @@
 var socket = io.connect('http://localhost:4200');
 
+function User() {
+  this.name;
+}
+
+var user = new User;
+
 socket.on('connect', function(data) {
-  var name = prompt("Who dis?");
+  var user_name = prompt("Who dis?");
+  user_name.length === 0
+    ? user.name = 'Anonymous'
+    : user.name = user_name;
   socket.emit('join', 'Hello World from ' + name);
 });
 
@@ -13,6 +22,6 @@ $('form').submit(function(e){
   e.preventDefault();
   var message = $('#chat_input').val();
   $('#chat_input').val('');
-  socket.emit('messages', {name: name,
-                           text: message});
+  socket.emit('messages', {name: user.name,
+                           message: message});
 });
